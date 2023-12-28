df7d52f0737d3226f66c5dd8e849d0ec3ceffc5d
ved-blacksof <ved.blacksof@gmail.com>
2023-12-28 17:59:32 +0530

    file text

--- app.jsx
+++ app.jsx
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "./screens")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.send("sdfjlfj");
 });
 
 app.get("/api", (req, res) => {
